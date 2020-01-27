c6ca52c1040325d7053a8c021091f95d8d36419f
LinxCoder <37672822+ceschmidt0001@users.noreply.github.com>
2020-01-27 18:31:39 -0500

    Figlet is green or should be

--- bot.js
+++ bot.js
@@ -774,7 +774,7 @@ client.on("message", async message => {
     }
     let strx = args.join(" ");
   let msg = require("child_process").execSync(`figlet ${strx}`).toString();
-   message.channel.send(`${msg}`, { code: "asciidoc"});
+   message.channel.send(`${msg}`, { code: "css"});
   }
   
     if (command === "update") {
