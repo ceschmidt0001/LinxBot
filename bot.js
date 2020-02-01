12147ab47cccaf28651d06f853f6de5e1c933c30
LinxCoder <zcamz@live,com>
2020-01-31 22:26:06 -0500

    colors atempt: TRES

--- bot.js
+++ bot.js
@@ -832,7 +832,7 @@ client.on("message", async message => {
     }
     let strx = args.join(" ");
     let msg = require("child_process").execSync(`figlet ${strx}`).toString();
-    message.channel.send(`${msg}`, { code: "md" });
+    message.channel.send(`${msg}`, { code: "tex" });
   }
 
   if (command === "banner") {
