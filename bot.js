80ead42a77e3bbce7deade4b0af043bf49ab9e3c
LinxCoder <zcamz@live,com>
2020-04-05 22:23:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -631,39 +631,6 @@ client.on("message", async message => {
     }
   }
 
-  /*if (command === "cmd") {
-    let ownerID = `${config.owner}`
-    if (message.author.id !== ownerID) {
-      message.channel.send("❌ This is a **BOT OWNER** Command");
-      return;
-    }
-    let code = args.join(" ");
-
-    const util = require('util');
-    const exec = util.promisify(require('child_process').exec);
-
-    async function ls(b) {
-      const {
-        stdout,
-        stderr
-      } = await exec(`${b}`);
-      if (`${stdout}` == "") {
-        if (`${stderr}` !== "") {
-          output = stderr;
-        } else {
-          output = "output: " + stdout;
-        }
-      } else {
-        output = "output: " + stdout;
-      }
-      if (`${stdout}` == "" | `${stderr}` == "") {
-        output = "output: " + stdout + "\n error: " + stderr;
-      }
-      return await message.channel.send("note: (ignore blank errors/outputs)\n" + `\`\`\`cmd\n${output}\n\`\`\``);
-    }
-    ls(`${code}`);
-  }*/
-
   if (command === "yiff") {
     if (message.channel.nsfw === false) {
       message.channel.send("⚠ This channel isn't marked as NSFW.");
@@ -798,7 +765,6 @@ client.on("message", async message => {
   }
 
   if (command === "cmd") {
-       message.channel.startTyping(3);
     if (message.author.id !== config.owner) {
       message.channel.send("❗ This is a **BOT OWNER** Command");
       return;
@@ -810,7 +776,6 @@ client.on("message", async message => {
     } catch (err) {
       await message.channel.send(`\`ERROR\` \`\`\`md\n${clean(err)}\n\`\`\``);
     }
-    message.channel.stopTyping(true);
   }
   if (command === "update") {
     if (message.author.id !== config.owner) {
